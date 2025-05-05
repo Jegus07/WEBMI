8435f54d608e88bb58d4bbe55415e47d4f1c0dfe
alex kouakou <jeguskouakou@gmail.com>
2025-05-05 14:05:55 +0100

    commit ajout de blog

--- WEBMI/WEBMI/settings.py
+++ WEBMI/WEBMI/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT depuis variable d'environnement, sinon par défaut
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
